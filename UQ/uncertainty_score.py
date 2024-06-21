40f0f0e0c28223913abf63b4f8bb54b4c3a22c34
jennyziyi-xu <ziyixu686@gmail.com>
2024-06-21 19:58:10 -0400

    add avg prob score

--- UQ/uncertainty_score.py
+++ UQ/uncertainty_score.py
@@ -18,25 +18,46 @@ with open(output_path, 'r') as file:
 for row in rows[1:]:
     dict_logits = ast.literal_eval(row[0])
     indices = ast.literal_eval(row[1])
+    #### Max Prob
     max_prob_para = []
     max_prob_sentence = float("-inf")
+
+    #### Avg Prob 
+    avg_prob_para = []
+    avg_prob_sentence = []
+
     for (token_pos, index) in enumerate(indices[1:-2]):
         # get the probability from dict_logits
         # this is softmax probability 
         prob = find_value(dict_logits[token_pos], index)
         # do negative log as shown in the paper. 
         neg_log_prob = -math.log(prob)
-        print("token_pos", token_pos, "prob",neg_log_prob)
+        # print("token_pos", token_pos, "prob",neg_log_prob)
 
+        #### Max Prob
         max_prob_sentence = max(max_prob_sentence, neg_log_prob)
+
+        #### Avg Prob
+        avg_prob_sentence.append(neg_log_prob)
 
         # A dot is represented by 29889. 
         if (index == 29889):
+            #### Max Prob 
             max_prob_para.append(max_prob_sentence)
             max_prob_sentence = float("-inf")
-    uncertainty_score = sum(max_prob_para) / len(max_prob_para)
 
-print(max_prob_para)
-print(uncertainty_score)
+            #### Avg Prob
+            avg_prob_sentence_score = sum(avg_prob_sentence)/len(avg_prob_sentence)
+            avg_prob_para.append(avg_prob_sentence_score)
+
+    ### Max Prob
+    score_max_prob = sum(max_prob_para) / len(max_prob_para)
+
+    ### Avg Prob 
+    score_avg_prob = sum(avg_prob_para) / len(avg_prob_para)
+
+print(avg_prob_para)
+print(score_avg_prob)
+
 
 
